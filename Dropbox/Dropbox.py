3a104262491788ca8dc7115cbd2320c62db01721
josevidal93 <josemanuelvj@hotmail.es>
2015-05-03 18:17:48 +0200

    Version Dropbox con error en la subida

--- Dropbox/Dropbox.py
+++ Dropbox/Dropbox.py
@@ -1,6 +1,11 @@
 # Include the Dropbox SDK libraries
 from dropbox import client, rest, session
- 
+from flask import Flask, request, render_template
+import os
+import json
+import httplib2
+
+app = Flask(__name__)
 # Get your app key and secret from the Dropbox developer website
 APP_KEY = 'qvkwb9hpaggsnac'
 APP_SECRET = 'jlv4jmek1t6j0j1'
@@ -10,58 +15,92 @@ ACCESS_TYPE = 'app_folder'
  
 sess = session.DropboxSession(APP_KEY, APP_SECRET, ACCESS_TYPE)
 request_token = sess.obtain_request_token()
-url = sess.build_authorize_url(request_token)
+
+
+# Para configurar Dropbox (autorizar).
+@app.route("/dropboxconfig")
+def DropboxAutentication():
+	url_autentication = sess.build_authorize_url(request_token)
+	return render_template('autentication.html',url=url_autentication)
+
+@app.route("/upload")
+def uploadFile():
+	return render_template('upload.html')
+	
+@app.route('/dropboxlist')
+def dropboxList():
+	return render_template('dropboxlist.html')
+
+
+
+
+
+########################################################################
+
+@app.route("/get_dropbox_auth", methods=['GET'])
+def getDropboxAuth():
+	url_autorizacion = sess.build_authorize_url(request_token)
+	return json.dumps('{\'url\': \'' + url_autorizacion + '\'}')
  
-# Make the user sign in and authorize this token
-print "url:", url
-print "Please visit this website and press the 'Allow' button, then hit 'Enter' here."
-raw_input()
  
-# This will fail if the user didn't visit the above URL
-access_token = sess.obtain_access_token(request_token)
+@app.route("/dropboxauthcode")
+def dropboxAuth():
+	access_token = sess.obtain_access_token(request_token)
+	sess.set_token(access_token.key,access_token.secret )
+	Client = client.DropboxClient(sess)
+	return json.dumps('{\'token_key\':\'' + access_token.key + ',\token_secret\':\'' + access_token.secret +',\client\':\'{\client_name\':\'' + Client.account_info()['display_name'] + '\'}' '\'}')
+@app.route("/get_client")
+def getclient():
+	Client = client.DropboxClient(sess)
+	return json.dumps('{\client_name\':\'' + Client.account_info()['display_name'] + '\'}')
+	
 
- 
-#Print the token for future reference
-print access_token.key
-print access_token.secret
+@app.route('/upload_to_dropbox', methods=['POST'])
+def savePostFile():
+		
+	#Obtenemos el nombre del fichero, que pondremos temporalmente en
+	#nuestra carpeta uploads/.
+	file = request.files['file']
+	file.save(os.path.join('uploads/', file.filename))
+	# Let's upload a file
+	
+	f = open(file.save,'rb')
+	response = client.put_file(file.save, f)
+	
+
+@app.route("/get_dropbox_list",methods=['GET'])
+def getDropboxList():
+	
+	Client = client.DropboxClient(sess)
+	folder_metadata = Client.metadata('/')
+	respuesta = []
+	
+	#  - Devuelve: JSON = [{
+#                         'id': file.id,
+#                         'filename': file.title,
+#                         'link': file.alternateLink,
+#                         'size': file.size (bytes)
+#                      }];
+	for elemento in folder_metadata['contents']:
+		respuesta.extend([{
+			'id': elemento['path'],
+			'filename': elemento['path'].replace('/',''),
+			'link': Client.share(elemento['path'])['url'],
+			'size':long(elemento['size'].replace('bytes',''))
+		}])
+	return json.dumps(respuesta)	
+
+		
 
 
-# We will use the OAuth token we generated already. The set_token API 
-# accepts the oauth_token and oauth_token_secret as inputs.
-sess.set_token(access_token.key,access_token.secret )
- 
-# Create an instance of the dropbox client for the session. This is
-# all we need to perform other actions
-client = client.DropboxClient(sess)
-print "linked account:", client.account_info()['display_name']
-
-# Let's upload a file
-f = open('pruebasubida.txt','rb')
-response = client.put_file('/pruebasubida.txt', f)
-print "uploaded:", response['path']
+@app.route("/get_quote")
+def getDropboxquote():
+	
+	Client = client.DropboxClient(sess)
+	return json.dumps('{\'Used\':\'' + str(Client.account_info()['quota_info']['normal']) + ',\Total\':\'' + str(Client.account_info()['quota_info']['quota']) + '\'}')
 
 
-#Metadata of full folder
-
-#folder_metadata = client.metadata('/')
-#print "metadata:", folder_metadata
-
-#Metadata of a given file
-
-#rev  = folder_metadata['contents'][1]['rev']
-#path = folder_metadata['contents'][1]['path']
-#print rev
-#print path
-
-#f, metadata = client.get_file_and_metadata(path,rev)
-
-
-#Let's download a file(share link, the 
-print client.share(response['path'])['url']
-
-#quota_info/normal	The user's used quota outside of shared folders (bytes).
-#quota_info/shared	The user's used quota in shared folders (bytes). If the user belongs to a team, this includes all usage contributed to the team's quota outside of the user's own used quota (bytes).
-#quota_info/quota	The user's total quota allocation (bytes). If the user belongs to a team, the team's total quota allocation (bytes).
-print client.account_info()['quota_info']
+if __name__ == "__main__":
+	app.run(debug=True)
 
 
